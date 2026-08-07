55336b7fb5db41dd69ceec2b3308b45dd8f75217
Jake <jakembry03@gmail.com>
2026-08-07 15:57:22 -0400

    Catches passive banners as well as HTTP and HTTPS banners

--- app/scanner.py
+++ app/scanner.py
@@ -1,4 +1,5 @@
 import socket
+import ssl
 from app.helpers import resolve_host
 
 
@@ -19,13 +20,30 @@ def scan_target(host: str, ports: range) -> list[dict]:
             result = sock.connect_ex((ip, port))
 
             if result == 0:
-                banner = grab_banner(sock)
+                if port == 80:
+                    banner = grab_http_banner(sock, ip)
+                    open_ports.append({
+                        "port": port,
+                        "status": "open",
+                        "banner": banner
+                    })
 
-                open_ports.append({
-                    "port": port,
-                    "status": "open",
-                    "banner": banner
-                })
+                elif port == 443:
+                    banner = grab_https_banner(sock, ip)
+                    open_ports.append({
+                        "port": port,
+                        "status": "open",
+                        "banner": banner
+                    })
+
+                else:   
+                    banner = grab_banner(sock)
+
+                    open_ports.append({
+                        "port": port,
+                        "status": "open",
+                        "banner": banner
+                    })
 
     return open_ports
 
@@ -55,6 +73,55 @@ def grab_banner(sock: socket.socket) -> str | None:
     text = "".join(c if c.isprintable() or c in "/t" else f"//x{ord(c):02x}" for c in text)
 
     return text.strip() or f"<{len(raw)} non-text bytes: {raw[:32].hex(' ')}>"
+
+def grab_http_banner(sock: socket.socket, host: str) -> str | None:
+    request = (
+        f"HEAD / HTTP/1.1\r\n"
+        f"Host: {host}\r\n"
+        f"Connection: close\r\n"
+        f"\r\n"
+    )
+
+    try:
+        sock.sendall(request.encode())
+
+        response = sock.recv(4096)
+
+        return response.decode(errors="ignore")
+
+    except (socket.timeout, OSError):
+        return None
+
+
+def grab_https_banner(sock: socket.socket, host: str) -> str | None:
+    context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+
+    # Useful for your own lab/network scanner where devices may have self-signed certificate
+    context.check_hostname = False
+    context.verify_mode = ssl.CERT_NONE
+
+    try:
+        tls_sock = context.wrap_socket(
+            sock,
+            server_hostname=host
+        )
+
+        request = (
+            f"HEAD / HTTP/1.1\r\n"
+            f"Host: {host}\r\n"
+            f"Connection: close\r\n"
+            f"\r\n"
+        )
+
+        tls_sock.sendall(request.encode())
+
+        response = tls_sock.recv(4096)
+
+        return response.decode(errors="ignore")
+
+    except (ssl.SSLError, socket.timeout, OSError):
+        return None
+    
 
 
 
--- docs/devNotes.md
+++ docs/devNotes.md
@@ -1,4 +1,4 @@
-V 0.1.0 = Basic single target port scanner
+v0.1.0 = Basic single target port scanner
 1. Initialize the Scanner
 2. Take host and port input (single, list, range)
 3. Scan the target
@@ -17,10 +17,25 @@ V 0.1.0 = Basic single target port scanner
 
 5. Output complete formatted results
 
-V 0.2.0
+v0.2.0
 1. Capture banners
     banner = receive all
     if banner has a value
         print banner
-2. Scan an entire network
+2. Have functionality for all major banners, HTTP/HTTPS and others.
+    if port == 80 or 443 then send http header or https header
+3. Scan an entire network
+    Would need a function that determines what format the given host is in. If it is in hostname, ip address, or network interface.
+    If hostname or IP, scan runs normally after resolving the name, if network interface, then the scan runs for the network
+    Then the same would be for a range of IP addresses
+
+v0.3.0
+1. Store results in SQLite database and read results from the database when outputting
+2. Grab OS and host information from the device if available
+
+v0.4.0
+1. Working command line functionality
+    Commands and flags, using krushnet as the executable command instead of python
+2. 
+
 
