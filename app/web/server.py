ea820047117fdbd973fee5fcd7d8146e1b7c14e6
Jake <jakembry03@gmail.com>
2026-08-10 17:07:00 -0400

    Added things

--- app/web/server.py
+++ app/web/server.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+
+app.mount(
+    "/static",
+    StaticFiles(directory="app/web/static"),
+    name="static"
+)
 
 templates = Jinja2Templates(
     directory="app/web/templates"
