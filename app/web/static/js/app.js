8f2687b044a5921ea12f5eb4fa6abb67fb6ada8a
Jake <jakembry03@gmail.com>
2026-08-12 15:46:51 -0400

    Got first official frontend to backend piece working

--- app/web/static/js/app.js
+++ app/web/static/js/app.js
@@ -1,9 +1,19 @@
 console.log("KRUSHNET JAVASCRIPT LOADED");
 
 async function updateBackendStatus() {
+    console.log("1. updateBackendStatus started");
     const response = await fetch("/api/status");
+    console.log("2. FastAPI responded");
+    console.log("HTTP status:", response.status);
     const data = await response.json();
+    console.log("3. JSON received:");
     console.log(data);
+    const statusText = document.getElementById("status-text");
+    console.log("4. HTML element found:");
+    console.log(statusText)
+    statusText.textContent = "Backend Online";
+    console.log("5. Text Changed");
+
 }
 
 updateBackendStatus();
